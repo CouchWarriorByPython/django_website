af5d372475eab1073646d9b5b2b4170abb22af73
CouchWarriorByPython <shkolatlen@gmail.com>
2021-10-09 14:30:06 +0300

    5 commit

--- crm/admin.py
+++ crm/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
-from .models import Order
+from .models import Order, StatusCrm, CommentCrm
 
 
-@admin.register(Order)
-class OrderAdmin(admin.ModelAdmin):
-    list_display = ('id', 'order_dt', 'order_name', 'order_phone')
-    list_filter = ('order_dt', 'order_name', 'order_phone')
+admin.site.register(Order)
+admin.site.register(StatusCrm)
+admin.site.register(CommentCrm)
+
+
 
 
 
--- landingwebsite/settings.py
+++ landingwebsite/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'crm.apps.CrmConfig',
-    'cms.apps.CmsConfig'
+    'cms.apps.CmsConfig',
+    'price.apps.PriceConfig'
 ]
 
 MIDDLEWARE = [
